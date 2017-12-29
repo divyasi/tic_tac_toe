31b2632dbca4429ce1a9db8bd43a0eb95bf17409
Divya Singh <ds492015@gmail.com>
2017-12-29 12:15:56 +0530

    moved winning combinaton and isSubset from player.js to utils.js

--- src/player.js
+++ src/player.js
@@ -1,20 +1,3 @@
-let winningCombinations = [
-  [1, 2, 3],
-  [4, 5, 6],
-  [7, 8, 9],
-  [1, 4, 7],
-  [2, 5, 8],
-  [3, 6, 9],
-  [1, 5, 9],
-  [3, 5, 7]
-];
-
-const isSubset = function(superSet, subSet) {
-  return subSet.every(function(element) {
-    return superSet.includes(element);
-  });
-};
-
 const Player = function(playerName, symbol) {
   this.name = playerName;
   this.symbol = symbol;
