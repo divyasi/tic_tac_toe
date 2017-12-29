ec047911c58911c307995cce7dca090b661ccdf7
Divya Singh <ds492015@gmail.com>
2017-12-29 12:11:39 +0530

    added a test hasMove

---
+++ test/playerTest.js
@@ -0,0 +1,12 @@
+let assert = require('chai');
+let player = require('../src/player.js');
+let test = {};
+
+test['hasMove should recongnise true wheather move is already present'] = function() {
+  let move = 1;
+  let moves = [1,2,3];
+  assert.isOk(hasMove(move),true);
+}
+
+
+exports.test = test;
